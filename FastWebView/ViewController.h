932e543190401c727c1fb16a4a53b7616292944c
胜任 <shengren.lsr@alibaba-inc.com>
2018-04-25 14:53:49 +0800

    Initial Commit

---
+++ FastWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastWebView
+//
+//  Created by 冷胜任 on 2018/4/25.
+//  Copyright © 2018年 taobao.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastWebView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FastWebView
+//
+//  Created by 冷胜任 on 2018/4/25.
+//  Copyright © 2018年 taobao.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
